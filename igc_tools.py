1bfc2d9a8abd320859991457ca0216b9b3734fe1
rbarlow <rbarlow@reliable.co>
2024-04-10 16:53:57 -0700

    Clean up tools, add gpx export

--- igc_tools.py
+++ igc_tools.py
@@ -5,16 +5,16 @@ import datetime
 
 @dataclass
 class BFix:
-    # UTC
-    # HHMMSS
-    time: datetime=datetime.datetime.now()
-    # decmal 
-    lat: float=0
+    """ BFix IGC Data Type
+    See https://xp-soaring.github.io/igc_file_format/igc_format_2008.html#link_4.1
+    """
 
+    time: datetime=datetime.datetime.now()
+    lat: float=0
     lon: float=0
     fix_validity: str=""
-    pressure_altitude: int=0
-    gnss_altitude: int=0
+    pressure_altitude_m: int=0
+    gnss_altitude_m: int=0
 
     def to_dict(self):
         return {
@@ -23,12 +23,15 @@ class BFix:
             "lat": self.lat,
             "lon": self.lon,
             "fix_validity": self.fix_validity,
-            "pressure_altitude": self.pressure_altitude,
-            "gnss_altitude": self.gnss_altitude,
+            "pressure_altitude_m": self.pressure_altitude_m,
+            "gnss_altitude_m": self.gnss_altitude_m,
         }
 
 
 def injest_igc_file(igc_file: str):
+    """ Parse a IGC file into header lines, fix lines and footer lines 
+    TODO: this could be much better
+    """
     header_lines = []
     content_lines = []
     footer_lines = []
@@ -61,11 +64,18 @@ def injest_igc_file(igc_file: str):
                 footer_lines.append(line)
     return (header_lines, content_lines, footer_lines)
 
-#B 235531 3440751N 11955269W A 00690 00732 54
-#B HHMMSS DDMMmmmN DDDMMmmmE V PPPPP GGGGG
+
 def parse_bfix(line: str):
+    """ Parse a b fix line to a BFix object
+        See https://xp-soaring.github.io/igc_file_format/igc_format_2008.html#link_4.1
+        Spec:    B HHMMSS DDMMmmmN DDDMMmmmE V PPPPP GGGGG CK
+        Example: B 235531 3440751N 11955269W A 00690 00732 54
+    """
+
     fix = BFix()
     assert(line[0] == "B")
+
+    # parse time to datetime
     hour = int(line[1:3])
     min = int(line[3:5])
     sec = int(line[5:7])
@@ -75,38 +85,28 @@ def parse_bfix(line: str):
     lat_degrees = int(line[7:9])
     lat_minutes = int(line[9:14])/1000
     north = line[14] == "N"
-    fix.lat = (lat_degrees + lat_minutes/60) * (1 if north else -1)
+    fix.lat = (lat_degrees + lat_minutes/60) 
+    fix.lat *= (1 if north else -1)
 
     # pull longitude
     lon_degrees = int(line[15:18])
     lon_minutes = int(line[18:23])/1000
 
-    east = line[23] == "E"
-    fix.lon = (lon_degrees + lon_minutes/60) * (1 if east else -1)
-    
+    east = (line[23] == "E")
+    fix.lon = (lon_degrees + lon_minutes/60) 
+    fix.lon *= (1 if east else -1)
     fix.fix_validity = line[24]
 
     # pressure altitude
-    fix.pressure_altitude = int(line[25:30])
+    fix.pressure_altitude_m = int(line[25:30])
     # gps altitude
-    fix.gnss_altitude = int(line[30:35])
+    fix.gnss_altitude_m = int(line[30:35])
     return fix
 
 def replace_gps_with_baro(line: str):
     # pressure altitude
-    pressure_altitude = line[25:30]
+    pressure_altitude_m = line[25:30]
 
     # gps altitude replacement
-    new_line = line[:30] + pressure_altitude + line[35:]
+    new_line = line[:30] + pressure_altitude_m + line[35:]
     return new_line
-
-
-def fix_robs_gps_issues(igc_file: str, new_igc_file: str = None):
-    if new_igc_file is None:
-        new_igc_file = igc_file[:-4] + "_fixed" + igc_file[-4:]
-    with open(igc_file, "r") as igc_file_lines, open(new_igc_file, "w+") as new_igc_file_lines:
-        for line in igc_file_lines:
-            if line[0] == "B":
-                new_igc_file_lines.write(replace_gps_with_baro(line))
-                continue
-            new_igc_file_lines.write(line)
--- math_utils.py
+++ math_utils.py
@@ -32,11 +32,56 @@ def dist_two_points(lat1, lon1, lat2, lon2):
         dist_ft = 2.0*SEMIMAJOR_AXIS_M*np.arcsin(np.sqrt(h))
         return dist_ft
 
-def build_distance_field(df):
+def build_direction_heading_fields(df):
     df["prev_lat"] = df["lat"].shift(periods = 1)
     df["prev_lon"] = df["lon"].shift(periods = 1)
-
+    
+    # Distance
     df = df.assign(distance_m=distance_between_points_m(df["lat"], df["lon"], df["prev_lat"], df["prev_lon"]))
+    
+    # Heading
+    delta_lon = (df["lon"] - df["prev_lon"])
+    y_component = np.sin(delta_lon) * np.cos(df["lat"])
+    x_component = np.cos(df["prev_lat"]) * np.sin(df["lat"]) - np.sin(df["prev_lat"]) \
+        * np.cos(np.sin(df["lat"])) * np.cos(delta_lon);    
+    brng = np.arctan2(y_component, x_component) #radians
+    brng = (brng + (2*np.pi)) % (2*np.pi)
+    df["direction_radians"] = brng
+
+
     df.drop(columns=['prev_lat', 'prev_lon'])
     return df
 
+
+def idx_min(x):
+    return x.index.values[np.argmin(x.values)]
+
+def idx_max(x):
+    return x.index.values[np.argmax(x.values)]
+
+
+def build_apparent_wind(df):
+    max_min = df['speed_ms'].rolling(30).agg([idx_max, 'max', idx_min, "min"])
+
+    min_dir = df["direction_radians"].iloc[max_min["idx_min"]]
+    max_dir = df["direction_radians"].iloc[max_min["idx_max"]]
+
+    max_min["max_dir"] = list(max_dir)
+    max_min["min_dir"] = list(min_dir)
+
+    max_min["vel"] = max_min["max"] - max_min["min"]
+
+    
+    # difference of direction between the max speed direction and min speed direction
+    max_min["seperation"] = max_min["max_dir"] - max_min["min_dir"]
+    # normalize to 0 - 2pi
+    max_min["seperation"] = (max_min["seperation"] + np.pi*2) % (np.pi*2)
+
+    # center about pi (180 degrees) and apply abs to normalized
+    max_min["seperation"] = np.abs(max_min["seperation"] - np.pi)
+
+    # look for times where the seperation is +- 22.5 degrees off of 180 (where we can)
+    max_min["filter"] = max_min["seperation"] < np.pi/4
+
+
+    return max_min
