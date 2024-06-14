c6c7ddf41e71cd36a150a4b429531daec0ecd39a
rbarlow <rbarlow@reliable.co>
2024-06-13 17:19:02 -0700

    Clean up for sharing out w/ friends

--- igc_tools.py
+++ igc_tools.py
@@ -1,20 +1,32 @@
-
+import pandas as pd
+import numpy as np
+import dateutil.parser.isoparser as isoparser
 from dataclasses import dataclass
 import datetime
+import gpxpy
+import os
+from simplekml import Kml, Color
+from copy import deepcopy
+
+
+import igc_tools
+import math_utils
+
+MS_TO_KMH = 3.6
 
 
 @dataclass
 class BFix:
-    """ BFix IGC Data Type
+    """BFix IGC Data Type
     See https://xp-soaring.github.io/igc_file_format/igc_format_2008.html#link_4.1
     """
 
-    time: datetime=datetime.datetime.now()
-    lat: float=0
-    lon: float=0
-    fix_validity: str=""
-    pressure_altitude_m: int=0
-    gnss_altitude_m: int=0
+    time: datetime = datetime.datetime.now()
+    lat: float = 0
+    lon: float = 0
+    fix_validity: str = ""
+    pressure_altitude_m: int = 0
+    gnss_altitude_m: int = 0
 
     def to_dict(self):
         return {
@@ -28,8 +40,8 @@ class BFix:
         }
 
 
-def injest_igc_file(igc_file: str):
-    """ Parse a IGC file into header lines, fix lines and footer lines 
+def ingest_igc_file(igc_file: str):
+    """Parse a IGC file into header lines, fix lines and footer lines
     TODO: this could be much better
     """
     header_lines = []
@@ -42,7 +54,7 @@ def injest_igc_file(igc_file: str):
 
         for line in igc_file:
             # remove the \n
-            line = line [:-1]
+            line = line[:-1]
             if header:
                 if line[0] == "B":
                     content_lines.append(line)
@@ -54,26 +66,28 @@ def injest_igc_file(igc_file: str):
                 if line[0] != "B":
                     if line[0] == "E":
                         continue
+                    if line[0] == "L":
+                        continue
                     contents = False
                     footer = True
                     footer_lines.append(line)
                     continue
                 content_lines.append(line)
                 continue
-            if footer: 
+            if footer:
                 footer_lines.append(line)
     return (header_lines, content_lines, footer_lines)
 
 
 def parse_bfix(line: str):
-    """ Parse a b fix line to a BFix object
-        See https://xp-soaring.github.io/igc_file_format/igc_format_2008.html#link_4.1
-        Spec:    B HHMMSS DDMMmmmN DDDMMmmmE V PPPPP GGGGG CK
-        Example: B 235531 3440751N 11955269W A 00690 00732 54
+    """Parse a b fix line to a BFix object
+    See https://xp-soaring.github.io/igc_file_format/igc_format_2008.html#link_4.1
+    Spec:    B HHMMSS DDMMmmmN DDDMMmmmE V PPPPP GGGGG CK
+    Example: B 235531 3440751N 11955269W A 00690 00732 54
     """
 
     fix = BFix()
-    assert(line[0] == "B")
+    assert line[0] == "B"
 
     # parse time to datetime
     hour = int(line[1:3])
@@ -81,20 +95,20 @@ def parse_bfix(line: str):
     sec = int(line[5:7])
     fix.time = datetime.time(hour=hour, minute=min, second=sec)
 
-    # pull latitude 
+    # pull latitude
     lat_degrees = int(line[7:9])
-    lat_minutes = int(line[9:14])/1000
+    lat_minutes = int(line[9:14]) / 1000
     north = line[14] == "N"
-    fix.lat = (lat_degrees + lat_minutes/60) 
-    fix.lat *= (1 if north else -1)
+    fix.lat = lat_degrees + lat_minutes / 60
+    fix.lat *= 1 if north else -1
 
     # pull longitude
     lon_degrees = int(line[15:18])
-    lon_minutes = int(line[18:23])/1000
+    lon_minutes = int(line[18:23]) / 1000
 
-    east = (line[23] == "E")
-    fix.lon = (lon_degrees + lon_minutes/60) 
-    fix.lon *= (1 if east else -1)
+    east = line[23] == "E"
+    fix.lon = lon_degrees + lon_minutes / 60
+    fix.lon *= 1 if east else -1
     fix.fix_validity = line[24]
 
     # pressure altitude
@@ -103,10 +117,187 @@ def parse_bfix(line: str):
     fix.gnss_altitude_m = int(line[30:35])
     return fix
 
-def replace_gps_with_baro(line: str):
-    # pressure altitude
-    pressure_altitude_m = line[25:30]
 
-    # gps altitude replacement
-    new_line = line[:30] + pressure_altitude_m + line[35:]
-    return new_line
+def kml_color_gradient_generator(a, r, g, b):
+    """Takes in alpha, red, green and blue values between 0-1 and returns a KML color string"""
+    return "%02x%02x%02x%02x" % (int(a * 255), int(b * 255), int(g * 255), int(r * 255))
+
+
+def speed_color_scale(speed):
+    """Takes in a normalized speed value (0-1) and returns an RGB color tuple."""
+    r, g, b = (0, 0, 0)
+
+    # Low range, turquise to blue
+    if speed < 0.5:
+        # scale to 0-1
+        speed *= 2
+
+        r = 0
+        g = 1 - speed
+        b = 1
+    # high range, blue to red
+    else:
+        speed = (speed - 0.5) * 2
+        r = speed
+        g = 0
+        b = 1 - speed
+
+    return (r, g, b)
+
+
+def thermal_color_scale(speed):
+    """
+    Takes in a normalized vertical speed value (0-1) and returns an RGB color tuple.
+    """
+    r, g, b = (0, 0, 0)
+
+    # Low range, blue to white
+    if speed < 0.5:
+        # scale to 0-1
+        speed *= 2
+
+        r = speed
+        g = speed
+        b = 1
+    # mid-high range, white to orange
+    elif speed < 0.75:
+        speed = (speed - 0.5) * 4
+        r = 1
+        g = 1 - (speed / 2)
+        b = 1 - speed
+    # high range, orange to red
+    else:
+        speed = (speed - 0.75) * 4
+        r = 1
+        g = 0.5 - (speed / 2)
+        b = 0
+
+    return (r, g, b)
+
+
+@dataclass
+class IGCLog:
+    file_path: str
+    header_info = None
+    # TODO: Parse headers. Specifically datetime so we can do timestamped kml tracks
+    footer_info = None
+    fixes: None
+    start_time: datetime.datetime = None
+    dataframe: pd.DataFrame = None
+
+    def __init__(self, file_path: str):
+        """Build an IGCLog file from a given file path"""
+        self.file_path = file_path
+        self.load_from_file()
+        self.build_computed_metrics()
+
+    def load_from_file(self):
+        """Using the object's file_path, load the igc file"""
+        self.fixes = []
+        self.header_info, fixes_list, self.footer_info = igc_tools.ingest_igc_file(self.file_path)
+
+        for fix in fixes_list:
+            self.fixes.append(igc_tools.parse_bfix(fix))
+        self.dataframe = pd.DataFrame.from_dict([bfix.to_dict() for bfix in self.fixes])
+
+    def build_computed_metrics(self):
+        self.dataframe["time_pandas"] = pd.to_datetime(self.dataframe["time_iso"], format="%H:%M:%S")
+
+        self.dataframe["seconds_delta"] = self.dataframe["time_pandas"].diff().dt.total_seconds()
+        self.dataframe = self.dataframe[self.dataframe["seconds_delta"] > 0]
+        self.dataframe["pressure_altitude_m_delta"] = self.dataframe["pressure_altitude_m"].diff()
+
+        self.dataframe["gnss_altitude_m_delta"] = self.dataframe["gnss_altitude_m"].diff()
+
+        self.dataframe["vertical_speed_ms"] = (
+            self.dataframe["pressure_altitude_m_delta"] / self.dataframe["seconds_delta"]
+        )
+        self.dataframe["vertical_speed_gnss_ms"] = (
+            self.dataframe["gnss_altitude_m_delta"] / self.dataframe["seconds_delta"]
+        )
+        self.dataframe["vertical_speed_gnss_average_ms"] = (
+            self.dataframe["vertical_speed_gnss_ms"].rolling(4, center=True).mean()
+        )
+
+        self.dataframe = math_utils.build_direction_heading_fields(self.dataframe)
+        self.dataframe["speed_ms"] = self.dataframe["distance_traveled_m_4s"] / (
+            self.dataframe["seconds_delta"].rolling(4).sum()
+        )
+        self.dataframe["speed_kmh"] = self.dataframe["speed_ms"] * MS_TO_KMH
+        self.dataframe["speed_kmh_average"] = self.dataframe["speed_ms"].rolling(20, center=True).mean() * MS_TO_KMH
+        self.dataframe["glide"] = self.dataframe["speed_ms"] / -self.dataframe["vertical_speed_gnss_average_ms"]
+        self.dataframe["glide"][self.dataframe["vertical_speed_gnss_average_ms"] > -0.4] = np.nan
+
+        self.dataframe["glide_60s"] = self.dataframe["glide"].rolling(120, center=True, min_periods=30).mean()
+        # remove intermediaries
+        self.dataframe.drop(
+            columns=[
+                "seconds_delta",
+                "pressure_altitude_m_delta",
+                "gnss_altitude_m_delta",
+            ]
+        )
+
+        self.dataframe.dropna()
+
+    def export_gpx(self, filename):
+        gpx_out = gpxpy.gpx.GPX()
+        # Create track:
+        gpx_track = gpxpy.gpx.GPXTrack()
+        gpx_out.tracks.append(gpx_track)
+        # Create segment in our track:
+        gpx_segment = gpxpy.gpx.GPXTrackSegment()
+        gpx_track.segments.append(gpx_segment)
+
+        for index, row in self.dataframe.iterrows():
+            gpx_segment.points.append(
+                gpxpy.gpx.GPXTrackPoint(row["lat"], row["lon"], elevation=row["pressure_altitude_m"])
+            )
+
+        with open(filename, "w") as xml:
+            xml.write(gpx_out.to_xml())
+
+    def export_kml_line(self, filename, track_type="Track", prefix=None, visibility=True):
+        kml = Kml()
+        multipnt = kml.newfolder(name=f"{prefix}{track_type}")
+        last_point = [0, 0, 0]
+        init = False
+
+        for idx, row in self.dataframe.iterrows():
+            longitude = row["lon"]
+            latitude = row["lat"]
+            altitude = row["gnss_altitude_m"]
+            if init == False:
+                init = True
+                last_point = [longitude, latitude, altitude]
+
+            current_point = [longitude, latitude, altitude]
+            ls = multipnt.newlinestring(coords=[current_point, last_point], altitudemode="absolute")
+
+            ls.style.linestyle.width = 2
+
+            if track_type == "Speed":
+                norm_val = math_utils.three_point_normalizer(row["speed_kmh_average"], 20, 35, 55)
+                r, g, b = speed_color_scale(norm_val)
+                ls.style.linestyle.color = kml_color_gradient_generator(1, r, g, b)
+
+            elif track_type == "VerticalSpeed":
+                norm_val = math_utils.three_point_normalizer(row["vertical_speed_gnss_average_ms"], -4, -1, 4)
+                r, g, b = thermal_color_scale(norm_val)
+                ls.style.linestyle.color = kml_color_gradient_generator(1, r, g, b)
+
+            else:
+                ls.style.linestyle.color = Color.red
+
+            ls.visible = visibility
+            last_point = current_point
+
+        kml.save(filename)
+
+    def export_tracks(self, file_prefix):
+        """Export both types of colored kml track
+        TODO: Merge into one KML file
+        """
+
+        self.export_kml_line(f"{file_prefix}_speed.kml", "Speed", prefix=file_prefix, visibility=False)
+        self.export_kml_line(f"{file_prefix}_vertical_speed.kml", "VerticalSpeed", prefix=file_prefix)
--- math_utils.py
+++ math_utils.py
@@ -2,86 +2,89 @@ import numpy as np
 import pandas as pd
 
 SEMIMAJOR_AXIS_M = 6378137.0
-DEGREES_TO_RADS = np.pi/180.0
+DEGREES_TO_RADS = np.pi / 180.0
 
-def haversine(theta):
-    return np.sin((theta)/2.0)**2
 
-def distance_between_points_m(lat1, lon1, lat2, lon2):
-    # Haversine formaula (https://en.wikipedia.org/wiki/Haversine_formula)
+def clip(value, low, high):
+    """ Clip the given value to be between the two bounds"""
+    if value < low:
+        return low
+    if value > high:
+        return high
+    return value
+
+
+def three_point_normalizer(value, bottom, midpoint, topend):
+    range_low = midpoint - bottom
+    range_high = topend - midpoint
+
+    # set NaN  to 0
+    if value != value:
+        value = midpoint
+
+    normalized_value = 0
+    if value > midpoint:
+        domain = value - midpoint
+        normalized_value = 0.5 + (domain / (2 * range_high))
+        #          (speed - midpoint)
+        #  0.5 + ----------------------
+        #         2 * (top - midpoint)
+    else:
+        domain = value - bottom
+        normalized_value = domain / (2 * range_low)
+        #   (speed - bottom)
+        # ----------------------
+        # 2 * (midpont - bottom)
+
+    normalized_value = clip(normalized_value, 0, 1)
+
+    return normalized_value
+
+def haversine(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
     lat1_rads = lat1 * DEGREES_TO_RADS
     lon1_rads = lon1 * DEGREES_TO_RADS
     lat2_rads = lat2 * DEGREES_TO_RADS
     lon2_rads = lon2 * DEGREES_TO_RADS
+    # haversine formula
+    dlon = lon2_rads - lon1_rads
+    dlat = lat2_rads - lat1_rads
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1_rads) * np.cos(lat2_rads) * np.sin(dlon / 2) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    r = 6371000  # Radius of earth in meters. Use 3956 for miles. Determines return value units.
+    return c * r
 
-    h = haversine(lat2_rads - lat1_rads) + \
-        np.cos(lat1_rads) * np.cos(lat2_rads) * haversine(lon2_rads - lon1_rads)    
-    
-    dist_m = 2.0 * SEMIMAJOR_AXIS_M *  np.arcsin(np.sqrt(h))
-    return dist_m
-
-def dist_two_points(lat1, lon1, lat2, lon2):
-        # Haversine formaula (https://en.wikipedia.org/wiki/Haversine_formula)
-        lat1 *= np.pi/180.0
-        lon1 *= np.pi/180.0
-        lat2 *= np.pi/180.0
-        lon2 *= np.pi/180.0
-        h = np.sin((lat2 - lat1)/2.0)**2 + np.cos(lat1) * \
-            np.cos(lat2)*np.sin((lon2 - lon1)/2.0)**2
-        
-        dist_ft = 2.0*SEMIMAJOR_AXIS_M*np.arcsin(np.sqrt(h))
-        return dist_ft
 
 def build_direction_heading_fields(df):
-    df["prev_lat"] = df["lat"].shift(periods = 1)
-    df["prev_lon"] = df["lon"].shift(periods = 1)
-    
+    """ Build out distance and heading fields from position data """
+    df["prev_lat_1s"] = df["lat"].shift(periods=1)
+    df["prev_lon_1s"] = df["lon"].shift(periods=1)
+    df["prev_lat_4s"] = df["lat"].shift(periods=4)
+    df["prev_lon_4s"] = df["lon"].shift(periods=4)
+
     # Distance
-    df = df.assign(distance_m=distance_between_points_m(df["lat"], df["lon"], df["prev_lat"], df["prev_lon"]))
-    
-    # Heading
-    delta_lon = (df["lon"] - df["prev_lon"])
-    y_component = np.sin(delta_lon) * np.cos(df["lat"])
-    x_component = np.cos(df["prev_lat"]) * np.sin(df["lat"]) - np.sin(df["prev_lat"]) \
-        * np.cos(np.sin(df["lat"])) * np.cos(delta_lon);    
-    brng = np.arctan2(y_component, x_component) #radians
-    brng = (brng + (2*np.pi)) % (2*np.pi)
-    df["direction_radians"] = brng
+    df = df.assign(distance_traveled_m_1s=haversine(df["lat"], df["lon"], df["prev_lat_1s"], df["prev_lon_1s"]))
+    df = df.assign(distance_traveled_m_4s=haversine(df["lat"], df["lon"], df["prev_lat_4s"], df["prev_lon_4s"]))
 
+    df["distance_traveled_m_16s"] = df["distance_traveled_m_4s"].rolling(4).sum()
+    df["distance_traveled_m_60s"] = df["distance_traveled_m_1s"].rolling(15).sum()
 
-    df.drop(columns=['prev_lat', 'prev_lon'])
+    # TODO: Heading
+
+    df.drop(columns=["prev_lat_1s", "prev_lon_1s", "prev_lat_4s", "prev_lon_4s"])
     return df
 
 
 def idx_min(x):
     return x.index.values[np.argmin(x.values)]
 
+
 def idx_max(x):
     return x.index.values[np.argmax(x.values)]
 
 
-def build_apparent_wind(df):
-    max_min = df['speed_ms'].rolling(30).agg([idx_max, 'max', idx_min, "min"])
-
-    min_dir = df["direction_radians"].iloc[max_min["idx_min"]]
-    max_dir = df["direction_radians"].iloc[max_min["idx_max"]]
-
-    max_min["max_dir"] = list(max_dir)
-    max_min["min_dir"] = list(min_dir)
-
-    max_min["vel"] = max_min["max"] - max_min["min"]
-
-    
-    # difference of direction between the max speed direction and min speed direction
-    max_min["seperation"] = max_min["max_dir"] - max_min["min_dir"]
-    # normalize to 0 - 2pi
-    max_min["seperation"] = (max_min["seperation"] + np.pi*2) % (np.pi*2)
-
-    # center about pi (180 degrees) and apply abs to normalized
-    max_min["seperation"] = np.abs(max_min["seperation"] - np.pi)
-
-    # look for times where the seperation is +- 22.5 degrees off of 180 (where we can)
-    max_min["filter"] = max_min["seperation"] < np.pi/4
-
-
-    return max_min
+# TODO: Apparent wind calc. See https://blueflyvario.blogspot.com/2012/09/calculating-wind-speed-from-gps-track.html
