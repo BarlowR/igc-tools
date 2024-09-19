eea704165fea9e41f56dc575f1178a4a546dc205
rbarlow <rbarlow@reliable.co>
2024-09-19 15:26:28 -0700

    * xctsk parsing tools * requirements freeze * README update

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# Tools for IGC analysis
+# Tools for IGC & xctsk files
 #### Rob Barlow, 2024-06
 
+
+## .xctsk Files
+
+### Convert to KML
+Run the following from the command line:
+```
+python3 xctsk_tools.py --in_file="<your_xctsk_file>.xctsk"
+```
+Or:
+```
+python3 xctsk_tools.py --in_file="<your_xctsk_filepath>.xctsk" --out_file="<your_kml_output_filepath>"
+```
+
+## .igc Files
 See igc_review.ipynb for examples
 
 ### Load IGC to Pandas Dataframe with key metrics calculated (Speed, Vertical Speed, Glide, Altidude, etc.)
@@ -14,3 +28,4 @@ See igc_review.ipynb for examples
 ### Export KML with color scale for horizontal speed
 
 ![speed](./assets/speed.png)
+
---
+++ xctsk_tools.py
@@ -0,0 +1,123 @@
+import json
+from simplekml import Kml, Color
+from polycircles import polycircles
+from dataclasses import dataclass
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--in_file', type=str, required=True)
+parser.add_argument('--out_file', type=str, required=False)
+
+@dataclass
+class xctsk_turnpoint:
+    """ Container for the turnpoints in xctsk files"""
+
+    radius: int = 0
+    type: str = None
+    altSmoothed: int = 0
+    description: str = ""
+    lat: int = 0
+    lon: int = 0
+    name: str = ""
+    order: int = 0
+
+    def load_from_dict(self, turnpoint_dict, order):
+        self.order = order
+        self.radius = turnpoint_dict["radius"]
+        if ("type" in turnpoint_dict.keys()):
+            self.type = turnpoint_dict["type"]
+        self.altSmoothed = turnpoint_dict["waypoint"]["altSmoothed"]
+        self.description = turnpoint_dict["waypoint"]["description"]
+        self.lat = turnpoint_dict["waypoint"]["lat"]
+        self.lon = turnpoint_dict["waypoint"]["lon"]
+        self.name = turnpoint_dict["waypoint"]["name"]
+    
+    def generate_name(self):
+        name = ""
+        if self.type:
+            return f"{self.type}, {self.name}"
+        else: 
+            return f"TP{self.order}, {self.name}"
+
+
+@dataclass
+class xctsk:
+    """xctsk Data Type from XContest
+    """
+    earth_model: str = ""
+    goal = {}
+    sss = {}
+    task_type:str = ""
+    turnpoints = []
+
+    def ingest_xctsk(self, xctsk_file: str):
+        """
+        Parse an xctsk. This is fairly easy since xctsk files are just Json files with a different extension.
+        """
+
+        with open(xctsk_file, "r") as xctsk_file:
+            xc_task = json.load(xctsk_file)
+
+            # Currently unused...
+            # self.earth_model = xc_task["earthModel"]
+            # self.goal = xc_task["goal"]
+            # self.sss = xc_task["sss"]conda ins
+            # self.task_type = xc_task["taskType"]
+
+            # load the tunpoints into objects
+            for index, tp in enumerate(xc_task["turnpoints"]):
+                next_turnpoint = xctsk_turnpoint()
+                next_turnpoint.load_from_dict(tp, index)
+                self.turnpoints.append(next_turnpoint)
+    
+    def export_to_kml(self, task_name):
+        kml_output = Kml()
+        last_tp = {"lat": None, "lon": None}
+        # iterate over the turnpoints
+        for turnpoint in self.turnpoints:
+            # Create a polycircle
+            polycircle = polycircles.Polycircle(latitude=turnpoint.lat,
+                                    longitude=turnpoint.lon,
+                                    radius=turnpoint.radius,
+                                    number_of_vertices=36)
+            # Create a polygon from the circle (google earth doesn't natively support circles)
+            polygon = kml_output.newpolygon(name=turnpoint.generate_name(),
+                                         outerboundaryis=polycircle.to_kml())
+
+            # Style the circle
+            polygon.style.linestyle.width = 2
+            polygon.style.linestyle.color = "FFFFFFFF"
+            # make the fill empty
+            polygon.style.polystyle.color = "00000000"
+            
+            # Create lines between the centers of the turnpoints
+            if last_tp["lat"]:
+                last_point = [last_tp["lon"], last_tp["lat"], 0]
+                current_point = [turnpoint.lon, turnpoint.lat, 0]
+                line = kml_output.newlinestring(coords=[current_point, last_point], altitudemode="relative")
+                line.style.linestyle.color = "FFFFFFFF"
+
+            last_tp["lat"] = turnpoint.lat
+            last_tp["lon"] = turnpoint.lon
+        
+        # Save the file
+        kml_output.save(f"{task_name}.kml")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    in_file = args.in_file
+    out_file = args.out_file
+
+    # Make the output file name the same as the input if none is given
+    if not out_file:
+        out_file = in_file[:-6]
+
+    # strip the ".kml" if it is passed in the string
+    elif out_file[-4:] == ".kml":
+        out_file = out_file[:-4]
+
+    task = xctsk()
+    task.ingest_xctsk(in_file)
+    task.export_to_kml(out_file)
